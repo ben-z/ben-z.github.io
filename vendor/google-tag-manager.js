f30187d6378b173512db435889ec1138b1f7ef3c
Ben Zhang <benzhangniu@gmail.com>
2019-10-06 17:06:43 -0400

    Add debug mode to gtm

--- vendor/google-tag-manager.js
+++ vendor/google-tag-manager.js
@@ -1,8 +1,8 @@
 (function (w, d, s, l, i) {
-w[l] = w[l] || []; w[l].push({
-    'gtm.start':
-        new Date().getTime(), event: 'gtm.js'
-}); var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-        'https://6t1dyd6xig.execute-api.us-east-1.amazonaws.com/default/*(d3d3Lmdvb2dsZXRhZ21hbmFnZXIuY29t)*/*(Z3Rt)*.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            ((w.urlParams && w.urlParams.ga_debug === 'true') ? 'https://www.googletagmanager.com/gtm.js?id=' : 'https://6t1dyd6xig.execute-api.us-east-1.amazonaws.com/default/*(d3d3Lmdvb2dsZXRhZ21hbmFnZXIuY29t)*/*(Z3Rt)*.js?id=') + i + dl; f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-T4M2DH9');
