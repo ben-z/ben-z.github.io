3313c787c5645bded230b47f16038c17801451c2
Ben Zhang <benzhangniu@gmail.com>
2019-10-06 18:04:06 -0400

    Use unicorn-proxy

--- vendor/google-tag-manager.js
+++ vendor/google-tag-manager.js
@@ -4,5 +4,5 @@
             new Date().getTime(), event: 'gtm.js'
     }); var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-            ((w.urlParams && w.urlParams.ga_debug === 'true') ? 'https://www.googletagmanager.com/gtm.js?id=' : 'https://6t1dyd6xig.execute-api.us-east-1.amazonaws.com/default/*(d3d3Lmdvb2dsZXRhZ21hbmFnZXIuY29t)*/*(Z3Rt)*.js?id=') + i + dl; f.parentNode.insertBefore(j, f);
+            ((w.urlParams && w.urlParams.ga_debug === 'true') ? 'https://www.googletagmanager.com/gtm.js?id=' : 'https://unicorn-proxy.benzhang.xyz/gtm/*(d3d3Lmdvb2dsZXRhZ21hbmFnZXIuY29t)*/*(Z3Rt)*.js?id=') + i + dl; f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-T4M2DH9');
